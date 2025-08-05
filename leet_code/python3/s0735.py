8c32be4a8a8926791bacf29808e89ab0accc98c6
Håkon Verås <hakon.veras@storebrand.no>
2025-08-05 20:13:54 +0200

    add: s0735.py

---
+++ leet_code/python3/s0735.py
@@ -0,0 +1,41 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for i in asteroids:
+            isDestroyed = False
+
+            while len(stack) != 0 and stack[-1] > 0 and i < 0:
+                if stack[-1] < abs(i):
+                    stack.pop()
+                elif stack[-1] == abs(i):
+                    stack.pop()
+                    isDestroyed = True
+                    break
+                else:
+                    isDestroyed = True
+                    break
+
+            if isDestroyed is False:
+                stack.append(i)
+
+        return stack
+
+
+class TestMySqrt(unittest.TestCase):
+    def test_example1(self):
+        self.assertEqual(Solution().asteroidCollision([5, 10, -5]), [5, 10])
+
+    def test_example2(self):
+        self.assertEqual(Solution().asteroidCollision([8, -8]), [])
+
+    def test_example3(self):
+        self.assertEqual(Solution().asteroidCollision([10, 2, -5]), [10])
+
+
+# This block allows you to run the tests directly from the script
+if __name__ == "__main__":
+    unittest.main(argv=["first-arg-is-ignored"], exit=False)
