592e11858e68801d2b112404674f624643be9b13
Håkon Verås <hakon.veras@storebrand.no>
2025-08-07 11:55:17 +0200

    add: s0055.py

---
+++ leet_code/python3/s0055.py
@@ -0,0 +1,27 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+            if max_reach >= len(nums) - 1:
+                return True
+        return False
+
+
+class TestMySqrt(unittest.TestCase):
+    def test_exampl1(self):
+        self.assertEqual(Solution().canJump([2, 3, 1, 1, 4]), True)
+
+    def test_example2(self):
+        self.assertEqual(Solution().canJump([3, 2, 1, 0, 4]), False)
+
+
+# This block allows you to run the tests directly from the script
+if __name__ == "__main__":
+    unittest.main(argv=["first-arg-is-ignored"], exit=False)
