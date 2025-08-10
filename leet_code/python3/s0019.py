b1f9aece1545bc9f66e7175aa379c00e62f9dc2d
Håkon Verås <hakon.veras@storebrand.no>
2025-08-10 20:42:01 +0200

    add: s0019.py

---
+++ leet_code/python3/s0019.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy_node = ListNode(0)
+        dummy_node.next = head
+        slow = dummy_node
+        fast = dummy_node
+
+        for i in range(n + 1):
+            fast = fast.next
+
+        while fast is not None:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+        return dummy_node.next
